670dfab5fe6173e98f0075c64544bea5bb251ce7
Jan Rychtar <honza.rychtar@gmail.com>
2020-05-25 13:17:32 +0200

    Zmena hlidani vyplneni jmena a hesla v dialogu proihlaseni

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,23 +10,32 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  state = 'init';
+  state = 'none';
+
+  error: string;
 
   constructor(private auth: AuthService, 
               private router: Router) { }
 
   ngOnInit() {
+    this.error = null;
   }
 
   onSubmit(form: NgForm) {
-    this.state = 'loading';
+    this.error = null;
     const username = form.value.username;
     const password = form.value.password;
+    if (!username || !password) {
+      this.error = "missing_credentials";
+      return;
+    }
+    this.state = 'loading';
     this.auth.login(username, password, (result) => {
+      this.state = 'none';
       if (result) {
         this.router.navigate(['/']);
       } else {
-        this.state = 'error';
+        this.error = "login_failed";
       }
     });
   }
