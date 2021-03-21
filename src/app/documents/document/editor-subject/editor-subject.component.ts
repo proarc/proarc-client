b4f2688d5ca3f06f6f7fd9e896c30cfa92abc9ed
Jan Rychtar <honza.rychtar@gmail.com>
2021-03-21 23:47:42 +0100

    Nacitani subjectu z autorit

--- src/app/documents/document/editor-subject/editor-subject.component.ts
+++ src/app/documents/document/editor-subject/editor-subject.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { CatalogDialogComponent } from 'src/app/dialogs/catalog-dialog/catalog-dialog.component';
+import { Metadata } from 'src/app/model/metadata.model';
 import { ElementField } from 'src/app/model/mods/elementField.model';
+import { ModsSubject } from 'src/app/model/mods/subject.model';
 import { CodebookService } from 'src/app/services/codebook.service';
 
 @Component({
@@ -14,10 +18,27 @@ export class EditorSubjectComponent implements OnInit {
   @Input() field: ElementField;
 
 
-  constructor(public codebook: CodebookService) {
+  constructor(public codebook: CodebookService, private dialog: MatDialog) {
   }
 
   ngOnInit() {
+  }
+
+  onLoadFromCatalog(item) {
+    const dialogRef = this.dialog.open(CatalogDialogComponent, { data: { type: 'authors' } });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && result['mods']) {
+        const mods = result['mods'];
+        console.log('mods', mods);
+        const metadata = new Metadata('', '', mods, 0);
+        const nameField = metadata.getField(ModsSubject.getSelector());
+        const items = nameField.getItems();
+        if (nameField && items.length > 0) {
+          this.field.addAfterItem(item, items[0]);
+          this.field.removeItem(item);
+        }
+      }
+    });
   }
 
 
