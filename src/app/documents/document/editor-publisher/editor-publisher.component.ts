2ec50aa1b1e34e0e6b81b8451e70c4a0d8f1f8a4
Jan Rychtar <honza.rychtar@gmail.com>
2021-04-04 19:31:26 +0200

    Zjednoduseni editoru publishera

--- src/app/documents/document/editor-publisher/editor-publisher.component.ts
+++ src/app/documents/document/editor-publisher/editor-publisher.component.ts
@@ -1,8 +1,6 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 import { ElementField } from 'src/app/model/mods/elementField.model';
-import { Translator } from 'angular-translator';
-import { HelpService } from 'src/app/services/help.service';
 
 @Component({
   selector: 'app-editor-publisher',
@@ -13,52 +11,17 @@ export class EditorPublisherComponent implements OnInit {
 
   @Input() field: ElementField;
 
-  private eventTypeCodes: string[] = ['', 'production', 'publication', 'distribution', 'manufacture', 'copyright'];
-  public eventTypes = [];
+  eventTypes: string[] = ['production', 'publication', 'distribution', 'manufacture', 'copyright'];
 
-  private issuanceCodes: string[] = ['', 'monographic', 'single unit', 'multipart monograph',
+  issuances: string[] = ['monographic', 'single unit', 'multipart monograph',
                                          'continuing', 'serial', 'integrating resource'];
-  public issuances = [];
 
-  public qualifierCodes: string[] = ['', 'approximate', 'inferred', 'questionable'];
-  public qualifiers = [];
+  qualifiers: string[] = ['approximate', 'inferred', 'questionable'];
 
-  constructor(public translator: Translator, public help: HelpService) {
-    this.translateCodes();
-    translator.languageChanged.subscribe(() => this.translateCodes());
+  constructor() {
   }
 
   ngOnInit() {
   }
-
-  translateCodes() {
-    this.translator.waitForTranslation().then(() => {
-      this.eventTypes = [];
-      for (const code of this.eventTypeCodes) {
-        if (code === '') {
-          this.eventTypes.push({ code: '', name: '-' });
-        } else {
-          this.eventTypes.push({ code: code, name: this.translator.instant('editor.publisher.event_type_code.' + code)});
-        }
-      }
-      this.issuances = [];
-      for (const code of this.issuanceCodes) {
-        if (code === '') {
-          this.issuances.push({ code: '', name: '-' });
-        } else {
-          this.issuances.push({ code: code, name: this.translator.instant('editor.publisher.issuance_code.' + code)});
-        }
-      }
-      this.qualifiers = [];
-      for (const code of this.qualifierCodes) {
-        if (code === '') {
-          this.qualifiers.push({ code: '', name: '-' });
-        } else {
-          this.qualifiers.push({ code: code, name: this.translator.instant('editor.publisher.date_qualifier_code.' + code)});
-        }
-      }
-    });
-  }
-
 
 }
