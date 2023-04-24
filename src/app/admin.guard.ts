82b5e87c52e950a3d663b81060ee21962d42d1db
alberto <alberto.hernandez@inovatika.cz>
2023-04-24 14:23:09 +0200

    Guard admin page

---
+++ src/app/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.user && this.authService.isAdmin()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+    
+  }
+
+}
