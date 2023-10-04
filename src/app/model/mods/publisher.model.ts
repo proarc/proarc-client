aa2c51f8b0234db82b5dca25077aea4eb58a7c17
albertoh <alberto.hernandez@inovatika.cz>
2023-10-04 11:32:47 +0200

    #416 controls replacement

--- src/app/model/mods/publisher.model.ts
+++ src/app/model/mods/publisher.model.ts
@@ -49,103 +49,96 @@ export class ModsPublisher extends ModsElement {
             //this.modsElement['publisher'] = ModsUtils.createEmptyField();
             this.modsElement['publisher'] = ModsUtils.createField(this, 'publisher');
         }
+        this.publisher = this.modsElement['publisher'][0];
         this.addControl('publisher');
+
 
         if (!this.modsElement['issuance']) {
             // this.modsElement['issuance'] = ModsUtils.createEmptyField();
             this.modsElement['issuance'] = ModsUtils.createField(this, 'issuance');
         }
+        this.issuance = this.modsElement['issuance'][0];
         this.addControl('issuance');
 
         if (!this.modsElement['dateIssued']) {
             this.modsElement['dateIssued'] = [];
+        }
+        if(this.available2('dateIssued')) {
+          this.dateIssueds = new ElementField(this.modsElement, ModsDateIssued.getSelector(), this.getField('dateIssued'));
+          this.addSubfield(this.dateIssueds);
         }
         this.addControl('dateIssued');
 
         if (!this.modsElement['dateOther']) {
             this.modsElement['dateOther'] = [];
         }
+        if (this.available2('dateOther')) {
+          this.dateOthers = new ElementField(this.modsElement, ModsDateOther.getSelector(), this.getField('dateOther'));
+          this.addSubfield(this.dateOthers);
+        }
         this.addControl('dateOther');
 
         if (!this.modsElement['copyrightDate']) {
             this.modsElement['copyrightDate'] = ModsUtils.createField(this, 'copyrightDate');;
         }
+        this.copyrightDate = this.modsElement['copyrightDate'][0];
         this.addControl('copyrightDate');
 
         if (!this.modsElement['dateCreated']) {
             this.modsElement['dateCreated'] = [];
+        }
+        if (this.available2('dateCreated')) {
+          this.dateCreateds = new ElementField(this.modsElement, ModsDateCreated.getSelector(), this.getField('dateCreated'));
+          this.addSubfield(this.dateCreateds);
         }
         this.addControl('dateCreated');
 
         if (!this.modsElement['dateValid']) {
             this.modsElement['dateValid'] = [];
         }
+        if (this.available2('dateValid')) {
+          this.dateValids = new ElementField(this.modsElement, ModsDateValid.getSelector(), this.getField('dateValid'));
+          this.addSubfield(this.dateValids);
+        }
         this.addControl('dateValid');
 
         if (!this.modsElement['dateCaptured']) {
             this.modsElement['dateCaptured'] = [];
+        }
+        if (this.available2('dateCaptured')) {
+          this.dateCaptureds = new ElementField(this.modsElement, ModsDateCaptured.getSelector(), this.getField('dateCaptured'));
+          this.addSubfield(this.dateCaptureds);
         }
         this.addControl('dateCaptured');
 
         if (!this.modsElement['dateModified']) {
             this.modsElement['dateModified'] = [];
         }
+        if (this.available2('dateModified')) {
+          this.dateModifieds = new ElementField(this.modsElement, ModsDateModified.getSelector(), this.getField('dateModified'));
+          this.addSubfield(this.dateModifieds);
+        }
         this.addControl('dateModified');
 
         if (!this.modsElement['place']) {
             this.modsElement['place'] = [];
         }
-        this.addControl('place');
-
-        this.publisher = this.modsElement['publisher'][0];
-        this.issuance = this.modsElement['issuance'][0];
-        this.copyrightDate = this.modsElement['copyrightDate'][0];
-
-        if(this.available2('frequency')) {
-            this.frequencies = new ElementField(this.modsElement, ModsFrequency.getSelector(), this.getField('frequency'));
-            this.addSubfield(this.frequencies);
-        this.addControl('frequency');
-        }
-
-        if(this.available2('dateIssued')) {
-          this.dateIssueds = new ElementField(this.modsElement, ModsDateIssued.getSelector(), this.getField('dateIssued'));
-          this.addSubfield(this.dateIssueds);
-        }
-
         if (this.available2('place')) {
           this.places = new ElementField(this.modsElement, ModsPlace.getSelector(), this.getField('place'));
           this.addSubfield(this.places);
         }
+        this.addControl('place');
 
-        if (this.available2('dateOther')) {
-          this.dateOthers = new ElementField(this.modsElement, ModsDateOther.getSelector(), this.getField('dateOther'));
-          this.addSubfield(this.dateOthers);
-        }
-
-        if (this.available2('dateCreated')) {
-          this.dateCreateds = new ElementField(this.modsElement, ModsDateCreated.getSelector(), this.getField('dateCreated'));
-          this.addSubfield(this.dateCreateds);
-        }
-
-        if (this.available2('dateValid')) {
-          this.dateValids = new ElementField(this.modsElement, ModsDateValid.getSelector(), this.getField('dateValid'));
-          this.addSubfield(this.dateValids);
-        }
-
-        if (this.available2('dateCaptured')) {
-          this.dateCaptureds = new ElementField(this.modsElement, ModsDateCaptured.getSelector(), this.getField('dateCaptured'));
-          this.addSubfield(this.dateCaptureds);
-        }
-
-        if (this.available2('dateModified')) {
-          this.dateModifieds = new ElementField(this.modsElement, ModsDateModified.getSelector(), this.getField('dateModified'));
-          this.addSubfield(this.dateModifieds);
+        if(this.available2('frequency')) {
+            this.frequencies = new ElementField(this.modsElement, ModsFrequency.getSelector(), this.getField('frequency'));
+            this.addSubfield(this.frequencies);
+            this.addControl('frequency');
         }
 
         if (this.available2('edition')) {
           this.editions = new ElementField(this.modsElement, ModsEdition.getSelector(), this.getField('edition'));
           this.addSubfield(this.editions);
-        this.addControl('edition');
+          this.addControl('edition');
         }
 
     }
