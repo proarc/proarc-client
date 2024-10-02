0a67e61ab49ee1d9c5bfcb9d243dbd76a14fd00a
LukasSykora <lukas.sykora@inovatika.cz>
2024-10-02 21:44:15 +0200

    Add donator DKRVO24-28

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -494,7 +494,7 @@ export class ConfigService {
 	public showFooter = !!APP_GLOBAL.showFooter;
 	public expandedModels = !!APP_GLOBAL.expandedModels;
 
-	private static donators = [ 'ilnorway', 'norway', 'dkrvo19-23'];
+	private static donators = [ 'ilnorway', 'norway', 'dkrvo19-23', 'dkrvo24-28'];
 	public donators = APP_GLOBAL.donators || ConfigService.donators;
 
 	public updateInSource = !!APP_GLOBAL.updateInSource;
