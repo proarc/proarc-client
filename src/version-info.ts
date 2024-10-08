a61b53996311f20f02a75549e65ab1715fddcae4
LukasSykora <lukas.sykora@inovatika.cz>
2024-10-08 10:28:03 +0200

    mods subject temporal

--- src/version-info.ts
+++ src/version-info.ts
@@ -1,9 +1,9 @@
 export const versionInfo = (() => {
-  try {
+  // try {
     // tslint:disable-next-line:no-var-requires
-    return require('../git-version.json');
-  } catch {
+    // return require('../git-version.json');
+  // } catch {
     // In dev the file might not exist:
-    return { tag: 'v0.0.0', hash: 'dev' };
-  }
+    return { tag: 'v2.2-PLUS', hash: 'dev', date: '2023-10-30T12:00:00.000Z' };
+  // }
 })();
