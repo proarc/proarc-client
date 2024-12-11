59696474c4462fa9259a797da810264a5b7c2b30
albertoh <alberto.hernandez@inovatika.cz>
2024-12-11 14:03:58 +0100

    #603

--- src/app/utils/uuid.ts
+++ src/app/utils/uuid.ts
@@ -1,6 +1,10 @@
 export class Uuid {
 
-    private static regexp = new RegExp('[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$');
+    // Client
+    // private static regexp = new RegExp('[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$');
+
+    // Jadro
+    private static regexp = new RegExp('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$');
 
 
     static validate(uuid: string): boolean {
